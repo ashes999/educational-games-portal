27ab61032780becf0f2923c38390796dbe4aa1da
ashes999 <ashes999@yahoo.com>
2015-11-17 18:08:01 -0500

    Keep list of all games in controller

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ angular.module('app').controller('appController', ['$scope', 'gamesRepository',
 function ($scope, repo) {
   $scope.currentTab = 'home';
   var allGames = repo.getData();
+  $scope.games = allGames;
   $scope.firstGame = allGames.shift();
   var midpoint = Math.ceil(allGames.length / 2);
   $scope.firstHalfGames = allGames.slice(0, midpoint);
