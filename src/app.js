9a64b65d3880206fa7763f086fc370917aafd875
ashes999 <ashes999@yahoo.com>
2015-11-18 17:18:56 -0500

    Added placeholders for filters + event

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ angular.module('app').controller('appController', ['$scope', 'gamesRepository',
 function ($scope, repo) {
   $scope.currentTab = 'home';
   var allGames = repo.getData();
+
   $scope.games = allGames;
   $scope.firstGame = allGames.shift();
   var midpoint = Math.ceil(allGames.length / 2);
@@ -24,5 +25,12 @@ function ($scope, repo) {
 
   this.selectImage = function(image) {
     $scope.selectedImage = image;
+  }
+
+  this.filterGames = function() {
+    var minGrade = $scope.minGrade || null;
+    var maxGrade = $scope.maxGrade || null;
+    var platformFilter = $scope.platformFilter || null;
+    console.log("From " + minGrade + " to " + maxGrade + " p=" + platformFilter);
   }
 }]);
